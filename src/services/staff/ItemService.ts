2d0bdb626ca2d378488c002246355728f37a63f0
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-07 10:58:18 +0000

    Added creation of item with ItemService

--- src/services/staff/ItemService.ts
+++ src/services/staff/ItemService.ts
@@ -1,8 +1,15 @@
-import { Types } from 'mongoose'
+import { HydratedDocument } from 'mongoose'
 import { IItem, Item } from '../../models/staff/Item'
 
 export default class ItemService {
-  async get (id: Types.ObjectId): Promise<IItem | null> {
-    return await Item.findOne({ _id: id })
+  async get (id: string): Promise<HydratedDocument<IItem> | null> {
+    return await Item.findById(id)
+  }
+
+  async create (name: string,
+    barcode: string,
+    position: string,
+    stock: number): Promise<HydratedDocument<IItem>> {
+    return await Item.create({ barcode, name, position, stock })
   }
 }
--- src/services/staff/__tests__/ItemService.spec.ts
+++ src/services/staff/__tests__/ItemService.spec.ts
@@ -1,20 +1,34 @@
-import { IItem, Item } from '../../../models/staff/Item'
-import { mock } from 'jest-mock-extended'
+import { Item } from '../../../models/staff/Item'
 import ItemService from '../ItemService'
-import { Types } from 'mongoose'
 
-jest.mock('../../../models/staff/Item', () => ({
-  Item: { findOne: jest.fn() }
-}))
-
-const mockFindOne = Item.findOne as jest.MockedFunction<typeof Item.findOne>
+// Mockingoose does not work with ES6 imports: https://stackoverflow.com/questions/70156753/typeerror-0-mockingoose-default-is-not-a-function-mockingooose
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mockingoose = require('mockingoose')
 
 describe('ItemService', () => {
   it('gets an item given an objectID', async () => {
-    const expectedItem = mock<IItem>()
-    mockFindOne.mockResolvedValue(expectedItem)
+    const expectedItem = new Item()
+    mockingoose(Item).toReturn(expectedItem, 'findOne')
     const service: ItemService = new ItemService()
-    const actual = await service.get(new Types.ObjectId())
+    const actual = await service.get(expectedItem.id)
     expect(actual).toEqual(expectedItem)
+  })
+
+  it('creates and saves an item', async () => {
+    const expectedName = 'name'
+    const expectedPosition = 'position'
+    const expectedBarcode = 'barcode'
+    const expectedStock = 0
+
+    mockingoose(Item)
+
+    const service = new ItemService()
+
+    const actual = await service.create(expectedName, expectedBarcode, expectedPosition, expectedStock)
+
+    expect(actual.name).toEqual(expectedName)
+    expect(actual.position).toEqual(expectedPosition)
+    expect(actual.barcode).toEqual(expectedBarcode)
+    expect(actual.stock).toEqual(expectedStock)
   })
 })

62235c78464ae9f7046d2435be56eb1ba0e55081
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-22 14:10:13 +0000

    Validated string fields against empty whitespace

--- src/setup/routes/itemRoutes.ts
+++ src/setup/routes/itemRoutes.ts
@@ -5,7 +5,9 @@ import validateRequest from './validateRequest'
 
 const router = Router()
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/create', body('stock').isInt({ min: 0 }), validateRequest, createPost)
+/* eslint-disable @typescript-eslint/no-misused-promises */
+router.post('/create', body(['name', 'barcode', 'position']).isLength({ min: 1 }).trim(),
+  body('stock').isInt({ min: 0 }), validateRequest, createPost)
+/* eslint-enable @typescript-eslint/no-misused-promises */
 
 export default router

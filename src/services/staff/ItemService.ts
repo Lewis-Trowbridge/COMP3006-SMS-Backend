20710f683e77a1c83348ca80da8291d188604cc0
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-22 18:11:20 +0000

    Added findByName method

--- src/services/staff/ItemService.ts
+++ src/services/staff/ItemService.ts
@@ -1,5 +1,6 @@
 import { HydratedDocument } from 'mongoose'
 import { IItem, Item } from '../../models/staff/Item'
+import escapeStringRegexp from 'escape-string-regexp'
 
 export default class ItemService {
   async get (id: string): Promise<HydratedDocument<IItem> | null> {
@@ -15,5 +16,10 @@ export default class ItemService {
 
   async findByBarcode (barcode: string): Promise<HydratedDocument<IItem> | null> {
     return await Item.findOne({ barcode })
+  }
+
+  async findByName (name: string): Promise<Array<HydratedDocument<IItem>> | null> {
+    const searchRegex = new RegExp(escapeStringRegexp(name) + '*.')
+    return await Item.find({ name: { $regex: searchRegex } }).limit(5)
   }
 }

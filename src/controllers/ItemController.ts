779835b4af226869a0cd9b8fd17fd5b4e37e92f8
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-22 18:54:02 +0000

    Added controller method for findByBarcode

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -1,7 +1,15 @@
 import ItemService from '../services/staff/ItemService'
 import { Request, Response } from 'express'
+import { ToObjectOptions } from 'mongoose'
 
 const service = new ItemService()
+
+const transformOptions: ToObjectOptions = {
+  transform: (doc, ret) => {
+    delete ret._id
+  },
+  versionKey: false
+}
 
 const createPost = async (req: Request, res: Response): Promise<void> => {
   const {
@@ -11,14 +19,20 @@ const createPost = async (req: Request, res: Response): Promise<void> => {
     stock
   } = req.body
   const model = await service.create(name, barcode, position, stock)
-  res.status(201).json(model.toObject({
-    transform: (doc, ret) => {
-      delete ret._id
-    },
-    versionKey: false
-  }))
+  res.status(201).json(model.toObject(transformOptions))
+}
+
+const findByBarcodeGet = async (req: Request<{}, {}, {}, { barcode: string }>, res: Response): Promise<void> => {
+  const { barcode } = req.query
+  const model = await service.findByBarcode(barcode)
+  if (model != null) {
+    res.status(200).json(model.toObject(transformOptions))
+  } else {
+    res.status(404)
+  }
 }
 
 export {
-  createPost
+  createPost,
+  findByBarcodeGet
 }
--- src/controllers/__tests__/ItemController.spec.ts
+++ src/controllers/__tests__/ItemController.spec.ts
@@ -1,6 +1,6 @@
 import ItemService from '../../services/staff/ItemService'
 import mocked = jest.mocked
-import { createPost } from '../ItemController'
+import { createPost, findByBarcodeGet } from '../ItemController'
 import { mock } from 'jest-mock-extended'
 import { Request, Response } from 'express'
 import { HydratedDocument } from 'mongoose'
@@ -11,47 +11,88 @@ jest.mock('../../services/staff/ItemService')
 const mockItemService = mocked(ItemService)
 
 describe('ItemController', () => {
-  it('calls ItemService with recieved values', async () => {
-    const expectedName = 'name'
-    const expectedBarcode = 'barcode'
-    const expectedPosition = 'position'
-    const expectedStock = 0
-    const expectedObject = {
-      barcode: expectedBarcode,
-      name: expectedName,
-      position: expectedPosition,
-      stock: expectedStock
-    }
-    const mockRequest = mock<Request>()
-    mockRequest.body = expectedObject
-    const mockResponse = mock<Response>({ status: jest.fn().mockReturnValue({ json: jest.fn() }) })
-    const expectedResponse = mock<HydratedDocument<IItem>>()
-    mockItemService.prototype?.create.mockResolvedValue(expectedResponse)
-    await createPost(mockRequest, mockResponse)
-    expect(mockItemService.prototype?.create).toHaveBeenCalledTimes(1)
-    expect(mockItemService.prototype?.create).toHaveBeenNthCalledWith(1, expectedName, expectedBarcode, expectedPosition, expectedStock)
-  })
+  describe('createPost', () => {
+    it('calls ItemService with received values', async () => {
+      const expectedName = 'name'
+      const expectedBarcode = 'barcode'
+      const expectedPosition = 'position'
+      const expectedStock = 0
+      const expectedObject = {
+        barcode: expectedBarcode,
+        name: expectedName,
+        position: expectedPosition,
+        stock: expectedStock
+      }
+      const mockRequest = mock<Request>()
+      mockRequest.body = expectedObject
+      const mockResponse = mock<Response>({ status: jest.fn().mockReturnValue({ json: jest.fn() }) })
+      const expectedResponse = mock<HydratedDocument<IItem>>()
+      mockItemService.prototype?.create.mockResolvedValue(expectedResponse)
+      await createPost(mockRequest, mockResponse)
+      expect(mockItemService.prototype?.create).toHaveBeenCalledTimes(1)
+      expect(mockItemService.prototype?.create).toHaveBeenNthCalledWith(1, expectedName, expectedBarcode, expectedPosition, expectedStock)
+    })
 
-  it('returns response from ItemService', async () => {
-    const expectedName = 'name'
-    const expectedBarcode = 'barcode'
-    const expectedPosition = 'position'
-    const expectedStock = 0
-    const expectedObject = {
-      barcode: expectedBarcode,
-      name: expectedName,
-      position: expectedPosition,
-      stock: expectedStock
-    }
-    const mockRequest = mock<Request>()
-    mockRequest.body = expectedObject
-    const expectedResponse = mock<HydratedDocument<IItem>>()
-    expectedResponse.toObject.mockReturnValue(expectedResponse)
-    mockItemService.prototype?.create.mockResolvedValue(expectedResponse)
-    const mockJsonFunc = jest.fn()
-    const mockResponse = mock<Response>({ status: jest.fn().mockReturnValue({ json: mockJsonFunc }) })
-    await createPost(mockRequest, mockResponse)
-    expect(mockJsonFunc).toHaveBeenCalledTimes(1)
-    expect(mockJsonFunc).toHaveBeenNthCalledWith(1, expectedResponse)
+    it('returns response from ItemService create', async () => {
+      const expectedName = 'name'
+      const expectedBarcode = 'barcode'
+      const expectedPosition = 'position'
+      const expectedStock = 0
+      const expectedObject = {
+        barcode: expectedBarcode,
+        name: expectedName,
+        position: expectedPosition,
+        stock: expectedStock
+      }
+      const mockRequest = mock<Request>()
+      mockRequest.body = expectedObject
+      const expectedResponse = mock<HydratedDocument<IItem>>()
+      expectedResponse.toObject.mockReturnValue(expectedResponse)
+      mockItemService.prototype?.create.mockResolvedValue(expectedResponse)
+      const mockJsonFunc = jest.fn()
+      const mockResponse = mock<Response>({ status: jest.fn().mockReturnValue({ json: mockJsonFunc }) })
+
+      await createPost(mockRequest, mockResponse)
+
+      expect(mockJsonFunc).toHaveBeenCalledTimes(1)
+      expect(mockJsonFunc).toHaveBeenNthCalledWith(1, expectedResponse)
+    })
+  })
+  describe('findByBarcodeGet', () => {
+    it('returns the value when a non-null value is returned from ItemService', async () => {
+      const expectedBarcode = 'barcode'
+      const mockRequest = mock<Request<{}, {}, {}, { barcode: string }>>()
+      mockRequest.query.barcode = expectedBarcode
+      const expectedResponse = mock<HydratedDocument<IItem>>()
+      expectedResponse.toObject.mockReturnValue(expectedResponse)
+      mockItemService.prototype.findByBarcode.mockResolvedValue(expectedResponse)
+      const mockJsonFunc = jest.fn()
+      const mockStatusFunc = jest.fn().mockReturnValue({ json: mockJsonFunc })
+      const mockResponse = mock<Response>({ status: mockStatusFunc })
+
+      await findByBarcodeGet(mockRequest, mockResponse)
+
+      expect(mockStatusFunc).toHaveBeenCalledTimes(1)
+      expect(mockStatusFunc).toHaveBeenNthCalledWith(1, 200)
+      expect(mockJsonFunc).toHaveBeenCalledTimes(1)
+      expect(mockJsonFunc).toHaveBeenNthCalledWith(1, expectedResponse)
+    })
+
+    it('returns 404 when a null value is returned from ItemService', async () => {
+      const expectedBarcode = 'barcode'
+      const mockRequest = mock<Request<{}, {}, {}, { barcode: string }>>()
+      mockRequest.query.barcode = expectedBarcode
+      const expectedResponse = null
+      mockItemService.prototype.findByBarcode.mockResolvedValue(expectedResponse)
+      const mockJsonFunc = jest.fn()
+      const mockStatusFunc = jest.fn().mockReturnValue({ json: mockJsonFunc })
+      const mockResponse = mock<Response>({ status: mockStatusFunc })
+
+      await findByBarcodeGet(mockRequest, mockResponse)
+
+      expect(mockStatusFunc).toHaveBeenCalledTimes(1)
+      expect(mockStatusFunc).toHaveBeenNthCalledWith(1, 404)
+      expect(mockJsonFunc).toHaveBeenCalledTimes(0)
+    })
   })
 })

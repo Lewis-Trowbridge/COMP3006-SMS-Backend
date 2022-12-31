efd9cd30abd23732b381f5a24086f685a86fbc80
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2022-12-31 19:17:25 +0000

    Tested creation of new items during resolve

--- src/services/customer/__tests__/ShoppingListService.spec.ts
+++ src/services/customer/__tests__/ShoppingListService.spec.ts
@@ -129,6 +129,54 @@ describe('ShoppingListService', () => {
       expect(fakeItem.items[0].toObject()).toEqual(newChanges)
       expect(fakeItem.items[1].toObject()).toEqual(newChanges2)
     })
+
+    it('adds new items', async () => {
+      const fakeItem = await ShoppingList.create({
+        created: new Date(),
+        ownerId: new Types.ObjectId().toString()
+      })
+      mockingoose(ShoppingList).toReturn(fakeItem, 'findOne')
+      const newChanges: IShoppingListItem = {
+        _id: new Types.ObjectId(),
+        quantity: 2,
+        text: 'newText'
+      }
+      
+      const service = new ShoppingListService()
+      await service.resolveChanges(fakeItem._id.toString(), [newChanges])
+
+      expect(fakeItem.items[0].toObject()).toEqual(newChanges)
+    })
+
+    it('adds new items and updates existing items at the same time', async () => {
+      const oldItem: IShoppingListItem = {
+        _id: new Types.ObjectId(),
+        quantity: 1,
+        text: 'oldText'
+      }
+      const fakeItem = await ShoppingList.create({
+        created: new Date(),
+        items: [oldItem],
+        ownerId: new Types.ObjectId().toString()
+      })
+      mockingoose(ShoppingList).toReturn(fakeItem, 'findOne')
+      const existingChanges: IShoppingListItem = {
+        _id: oldItem._id,
+        quantity: 2,
+        text: 'newText'
+      }
+      const newChanges: IShoppingListItem = {
+        _id: new Types.ObjectId(),
+        quantity: 2,
+        text: 'newText'
+      }
+      
+      const service = new ShoppingListService()
+      await service.resolveChanges(fakeItem._id.toString(), [newChanges, existingChanges])
+
+      expect(fakeItem.items[0].toObject()).toEqual(existingChanges)
+      expect(fakeItem.items[1].toObject()).toEqual(newChanges)
+    })
   })
 })
 

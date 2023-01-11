a75dc10e1d936c8dae68ee8e47748a12e719236e
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-01-11 20:12:29 +0000

    Added service verify method

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,4 @@
-import { hash } from 'argon2'
+import { hash, verify } from 'argon2'
 import { HydratedDocument } from 'mongoose'
 import { IUser, User, UserType } from '../models/User'
 
@@ -6,6 +6,14 @@ export default class UserService {
   async create (username: string, password: string, type = UserType.Customer): Promise<HydratedDocument<IUser>> {
     const hashedPassword = await this.hashPassword(password)
     return await User.create({ password: hashedPassword, type, username })
+  }
+
+  async verify (username: string, password: string): Promise<boolean> {
+    const hash = (await User.findOne({ username }))?.password
+    if (hash != null) {
+      return await verify(hash, password)
+    }
+    return false
   }
 
   private async hashPassword (password: string): Promise<string> {

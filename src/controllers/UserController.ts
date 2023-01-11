43b9666a8277bb97b10baf0250111b4e62a58c64
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-01-11 17:35:31 +0000

    Prevented exposure of user data in create

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express'
-import { mongoExcludeVersionToObjectOptions } from '../constants'
 import { UserType } from '../models/User'
 import UserService from '../services/UserService'
 
@@ -10,8 +9,8 @@ const createPost = async (req: Request, res: Response): Promise<void> => {
     username,
     password
   } = req.body
-  const user = await service.create(username, password, UserType.Customer)
-  res.status(201).json(user.toObject(mongoExcludeVersionToObjectOptions))
+  await service.create(username, password, UserType.Customer)
+  res.sendStatus(201)
 }
 
 export {
